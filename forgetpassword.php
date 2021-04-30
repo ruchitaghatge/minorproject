d7c97be617205d5d6a2b79e9cf7054757806dc87
ruchitaghatge <ruchitaghatge.mca19@rvce.edu.in>
2021-04-30 20:13:43 +0530

    forget password

---
+++ forgetpassword.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<script>
+</script>
+<title>Reset Password</title>
+
+</head>
+<body>
+<h1>Forgot password</h1>
+<hr>
+<div style="position:absolute; top:200; left:400;">
+<form method="post" action="resetpass.php">
+Enter username:<input type=text name="username" required title="E-mail format must be someone@example.com" ></br></br>
+Enter your phone number : <input type=text name="phoneno" required pattern="[0-9]{10}" title"Phone number of 10 digits"></br></br>
+<input type="submit" value="submit">
+</form>
+</div>
+ <img src="images/forgotpassword.jpg" height="500" width="500">
+</body>
+</html>
+
